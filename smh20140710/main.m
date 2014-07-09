5a03259554a9c78d0b9e94067a727c4968872f9a
smh20140705 <smh20140705@gmail.com>
2014-07-09 17:00:13 +0100

    Initial Commit

---
+++ smh20140710/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  smh20140710
+//
+//  Created by Shah Hussain on 09/07/2014.
+//  Copyright (c) 2014 smh20140705. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMHAppDelegate class]));
+    }
+}
